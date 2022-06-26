5db00cbb3c2b023daa1e802fd81c18d2638a1c4a
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-06-26 23:28:51 +0300

    Update Program.cs

--- Task3.4/Program.cs
+++ Task3.4/Program.cs
@@ -6,36 +6,37 @@ namespace Task3._4
     {
         static void Main(string[] args)
         {
-            bool isWorking = true;
-            int sizeOfArray = 10;
+           bool isWorking = true;
             string input;
-            int number;
-            int sum = 0;
-            int repeat = 0;
-            int[] arrayOfNumbers = new int[sizeOfArray];
-            Console.WriteLine("sum для сложения\nexit для выхода\n\nВводите числа");
+            int[] arrayOfNumbers = new int[0];
+            Console.WriteLine("sum для сложения\nexit для выхода\n");
 
             while (isWorking)
             {
+
                 switch (input = Console.ReadLine().ToLower().Trim())
                 {
                     case "sum":
+                        int sum = 0;
 
                         for (int i = 0; i < arrayOfNumbers.Length; i++)
                         {
                             sum += arrayOfNumbers[i];
                         }
 
-                        Console.WriteLine(sum + " sum");
+                        Console.WriteLine($"Сумма введеных чисел: {sum}\n");
                         break;
                     case "exit":
                         isWorking = false;
-                        Console.Clear();
                         break;
                     default:
-                        number = int.Parse(input);
-                        arrayOfNumbers[repeat] = number;
-                        repeat++;
+                        int[] copyArray = new int[arrayOfNumbers.Length + 1];
+                        int number = Convert.ToInt32(input);
+                        copyArray[copyArray.Length - 1] = number;
+
+                        for (int i = 0; i < arrayOfNumbers.Length; i++)
+                            copyArray[i] = arrayOfNumbers[i];
+                        arrayOfNumbers = copyArray;
                         break;
                 }
             }
